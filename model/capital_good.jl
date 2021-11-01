9f1d711b089705793761a09eefd8476e38a167ab
Joos Akkerman <akkermanjoos@gmail.com>
2021-11-01 22:44:52 +0100

    Added replacement and expansion planning, changed variable names, sends brochures

--- model/capital_good.jl
+++ model/capital_good.jl
@@ -6,14 +6,15 @@ using StatsBase
 
 mutable struct CapitalGoodProducer 
     id :: Int
-    A_t :: Array{Float64}
-    B_t :: Array{Float64}
-    p_t :: Array{Float64}
-    c_t :: Array{Float64}
-    RD_t :: Array{Float64}
-    IM_t :: Array{Float64}
-    IN_t :: Array{Float64}
-    S_t :: Array{Float64}
+    A :: Array{Float64}
+    B :: Array{Float64}
+    p :: Array{Float64}
+    c :: Array{Float64}
+    RD :: Array{Float64}
+    IM :: Array{Float64}
+    IN :: Array{Float64}
+    S :: Array{Float64}
+    HC :: Array{Float64}
 end
 
 
@@ -24,10 +25,10 @@ function innovate!(capital_good_producer, global_param, macro_struct, model_stru
     """
 
     set_RD(capital_good_producer, global_param, macro_struct)
-    tech_choices = [(capital_good_producer.A_t[end], capital_good_producer.B_t[end])]
+    tech_choices = [(capital_good_producer.A[end], capital_good_producer.B[end])]
 
     # determine innovation of machines
-    θ_IN = 1 - exp(-global_param.ζ * capital_good_producer.IN_t[end])
+    θ_IN = 1 - exp(-global_param.ζ * capital_good_producer.IN[end])
     if (Bernoulli(θ_IN) == 1)
         A_t_in = update_At(capital_good_producer, global_param)
         B_t_in = update_Bt(capital_good_producer, global_param)
@@ -35,7 +36,7 @@ function innovate!(capital_good_producer, global_param, macro_struct, model_stru
     end
 
     # determine immitation of competitors
-    θ_IM = 1 - exp(-global_param.ζ * capital_good_producer.IM_t[end])
+    θ_IM = 1 - exp(-global_param.ζ * capital_good_producer.IM[end])
     if (Bernoulli(θ_IM) == 1)
         A_t_im, B_t_im = imitate_technology(capital_good_producer, model_struct)
         push!(tech_choices, (A_t_im, B_t_im))
@@ -44,16 +45,37 @@ function innovate!(capital_good_producer, global_param, macro_struct, model_stru
     # make choice between possible technologies
     if length(tech_choices) == 1
         # if no new technologies, keep current technologies
-        push!(capital_good_producer.A_t, capital_good_producer.A_t[end])
-        push!(capital_good_producer.B_t, capital_good_producer.B_t[end])
+        push!(capital_good_producer.A, capital_good_producer.A[end])
+        push!(capital_good_producer.B, capital_good_producer.B[end])
     else
         c_h = map(x -> global_param.w[end]/x[0], tech_choices)
         p_h = map(x -> (1 + global_param.μ1)*x, c_h)
         r_h = p_h + global_param.b * c_h
         index = argmin(r_h)
-        push!(capital_good_producer.A_t, tech_choices[index][0])
-        push!(capital_good_producer.B_t, tech_choices[index][1])
+        push!(capital_good_producer.A, tech_choices[index][0])
+        push!(capital_good_producer.B, tech_choices[index][1])
     end
+
+end
+
+
+function send_brochures(capital_good_producer, model_struct, global_param)
+    # TODO: reset brochures of consumer good producers each turn
+
+    # set up brochure
+    brochure = (capital_good_producer, capital_good_producer.p[end], capital_good_producer.c[end])
+
+    # send brochure to historical clients
+    for client in capital_good_producer.HC
+        push!(client.brochures, brochure)
+    end
+
+    # select new clients, send brochure
+    NC_potential = setdiff(model_struct.consumer_good_producers, consumer_good_producer.HC)
+    indexes = rand(1:length(NC_potential), round(global_param.γ * length(consumer_good_producer.HC)))
+    for index in indexes:
+        push!(NC_potential[index].brochures, brochure)
+    end 
 
 end
 
@@ -68,8 +90,8 @@ function imitate_technology(capital_good_producer, model_struct, F1)
     
     index = sample(1:F1, Weights(weights))
 
-    A_t_im = model_struct.capital_good_producers[index].A_t[end]
-    B_t_im = model_struct.capital_good_producers[index].B_t[end]
+    A_t_im = model_struct.capital_good_producers[index].A[end]
+    B_t_im = model_struct.capital_good_producers[index].B[end]
     
     return A_t_im, B_t_im
 end
@@ -77,34 +99,35 @@ end
 
 function update_At!(capital_good_producer, global_param)
     """
-    Updates A_t
+    Updates A
     """
     κ_A = Beta(global_param.α1, global_param.β1)
-    A_t_in = A_t[end]*(1 + κ_A)
+    A_t_in = A[end]*(1 + κ_A)
     return A_t_in
 end
 
 
 function update_Bt!(capital_good_producer, global_param) 
     κ_A = Beta(global_param.α1, global_param.β1)
-    B_t_in = B_t[end]*(1 + κ_A)
+    B_t_in = B[end]*(1 + κ_A)
     return B_t_in
 end
 
 
-function set_price(capital_good_producer, global_param, macro_struct)
-    c_t = macro_struct.w[end] / capital_good_producer.B_t[end]
-    p_t_new = (1 + global_param.μ1)c_t
-    push!(capital_good_producer.p_t, p_t_new)
+function set_price!(capital_good_producer, global_param, macro_struct)
+    c_t = macro_struct.w[end] / capital_good_producer.B[end]
+    p_t = (1 + global_param.μ1) * c_t
+    push!(capital_good_producer.c, c_t)
+    push!(capital_good_producer.p, p_t)
 end
 
 
-function set_RD(capital_good_producer, global_param, macro_struct)
-    RD_t_new = global_param.ν*(capital_good_producer.S_t[end])
-    push!(capital_good_producer.RD_t, RD_t_new)
+function set_RD!(capital_good_producer, global_param, macro_struct)
+    RD_t_new = global_param.ν*(capital_good_producer.S[end])
+    push!(capital_good_producer.RD, RD_t_new)
 
     IN_t_new = global_param.ξ*RD_t_new
     IM_t_new = (1 - global_param.ξ)*RD_t_new
-    push!(capital_good_producer.IN_t, IN_t_new)
-    push!(capital_good_producer.IM_t, IN_t_new)
+    push!(capital_good_producer.IN, IN_t_new)
+    push!(capital_good_producer.IM, IN_t_new)
 end
--- model/consumer_good.jl
+++ model/consumer_good.jl
@@ -1,20 +1,58 @@
 
+mutable struct Machine
+    A :: Float64
+    B :: Float64
+    c :: Float64
+end
+
 mutable struct ConsumerGoodProducer 
     id :: Int
-    A :: Array{Float64}
-    B :: Array{Float64}
+    # A :: Array{Float64}
+    # B :: Array{Float64}
     p :: Array{Float64}
     c :: Array{Float64}
     RD :: Array{Float64}
     D :: Array{Float64}
     N :: Array{Float64}
     K :: Array{Float64}
+    I :: Array{Float64}
+    Ksi :: Array
+    brochures :: Array
 end
 
-function plan_production(consumer_good_producer, global_param, model_struct)
+
+function plan_investment!(consumer_good_producer, global_param, model_struct)
+
+    # choose capital goods producer
+    
+
+    # compute investment
+    EId = plan_expansion(consumer_good_producer, global_param, model_struct)
+    RS = plan_replacement(consumer_good_producer, global_param, p_star, c_star)
+    I_t = EId + RS
+    push!(consumer_good_producer, EId, RS)
+end
+
+
+function plan_expansion(consumer_good_producer, global_param, model_struct)
     Nd = global_param.ι * consumer_good_producer.D[end]
     Qd = consumer_good_producer.D[end] + Nd - consumer_good_producer.N[end]
     Kd = Qd / consumer_good_producer.A[end]
     # TODO: check if this is the correct function
     EId = Kd - consumer_good_producer.K[end]
+
+    return EId
+end
+
+
+function plan_replacement(consumer_good_producer, global_param, p_star, c_star)
+    RS = Array{Float64}
+
+    for machine in consumer_good_producer.Ksi
+        if (p_star/(machine.A - c_star) <= global_param.b)
+            push!(RS, machine)
+        end
+    end
+
+    return RS
 end
