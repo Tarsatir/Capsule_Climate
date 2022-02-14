7f04b026b260f28aa95da1c77c5a0958f5aa0e36
Joos Akkerman <akkermanjoos@gmail.com>
2022-02-14 18:34:24 +0100

    Revised errors, tried to fix some bugs

--- plotting/plot_final_dists.py
+++ plotting/plot_final_dists.py
@@ -9,13 +9,18 @@ def plot_final_dist():
 
     # print(df.head())
 
-    fig, [ax1, ax2]= plt.subplots(1,2, figsize=(10,5))
+    fig, [ax1, ax2, ax3]= plt.subplots(1, 3, figsize=(12,5))
 
     ax1.hist(df.all_I)
-    ax1.set_title('Income')
+    ax1.set_title('Income ($I_{i,t}$)')
 
     ax2.hist(df.all_w)
-    ax2.set_title('Wages')
+    ax2.set_title('Wages ($w_{i,t}$)')
+
+    ax3.hist(df.all_W, bins=100)
+    ax3.set_title('Wealth ($W_{i,t}$)')
+
+    plt.tight_layout()
 
     plt.savefig('plots/final_dist.png')
 
